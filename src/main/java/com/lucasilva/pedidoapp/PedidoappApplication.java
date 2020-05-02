fb0648b498f05b3d34d6891e48662a2220110fe8
Lucas Silva <lucas.s.l75@hotmail.com>
2020-05-02 01:32:46 -0300

    projeto criado

---
+++ src/main/java/com/lucasilva/pedidoapp/PedidoappApplication.java
@@ -0,0 +1,13 @@
+package com.lucasilva.pedidoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasilva/pedidoapp/PedidoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasilva.pedidoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
