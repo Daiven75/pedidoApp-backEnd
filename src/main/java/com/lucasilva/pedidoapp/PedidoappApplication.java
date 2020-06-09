dce466a02f14a94f184915b16f4db6a0032ae665
Lucas Silva <lucas.s.l75@hotmail.com>
2020-06-09 00:24:34 -0300

    salvando primeiro arquivo no S3

--- src/main/java/com/lucasilva/pedidoapp/PedidoappApplication.java
+++ src/main/java/com/lucasilva/pedidoapp/PedidoappApplication.java
@@ -1,38 +1,17 @@
 package com.lucasilva.pedidoapp;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.lucasilva.pedidoapp.domain.Categoria;
-import com.lucasilva.pedidoapp.domain.Cidade;
-import com.lucasilva.pedidoapp.domain.Cliente;
-import com.lucasilva.pedidoapp.domain.Endereco;
-import com.lucasilva.pedidoapp.domain.Estado;
-import com.lucasilva.pedidoapp.domain.ItemPedido;
-import com.lucasilva.pedidoapp.domain.Pagamento;
-import com.lucasilva.pedidoapp.domain.PagamentoComBoleto;
-import com.lucasilva.pedidoapp.domain.PagamentoComCartao;
-import com.lucasilva.pedidoapp.domain.Pedido;
-import com.lucasilva.pedidoapp.domain.Produto;
-import com.lucasilva.pedidoapp.domain.enums.EstadoPagamento;
-import com.lucasilva.pedidoapp.domain.enums.TipoCliente;
-import com.lucasilva.pedidoapp.repositories.CategoriaRepository;
-import com.lucasilva.pedidoapp.repositories.CidadeRepository;
-import com.lucasilva.pedidoapp.repositories.ClienteRepository;
-import com.lucasilva.pedidoapp.repositories.EnderecoRepository;
-import com.lucasilva.pedidoapp.repositories.EstadoRepository;
-import com.lucasilva.pedidoapp.repositories.ItemPedidoRepository;
-import com.lucasilva.pedidoapp.repositories.PagamentoRepository;
-import com.lucasilva.pedidoapp.repositories.PedidoRepository;
-import com.lucasilva.pedidoapp.repositories.ProdutoRepository;
+import com.lucasilva.pedidoapp.services.S3Service;
 
 @SpringBootApplication
 public class PedidoappApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoappApplication.class, args);
@@ -40,5 +19,6 @@ public class PedidoappApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\parque.jpg");
 	}
 }
--- src/main/java/com/lucasilva/pedidoapp/domain/ItemPedido.java
+++ src/main/java/com/lucasilva/pedidoapp/domain/ItemPedido.java
@@ -2,7 +2,6 @@ package com.lucasilva.pedidoapp.domain;
 
 import java.io.Serializable;
 import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
